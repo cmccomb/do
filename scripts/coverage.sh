72a8043dd4c5fc98a483b5170bbf10ff7fe3812a
Chris McComb <ccmcc2012@gmail.com>
2025-12-10 20:29:27 -0500

    More sh less bats

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -28,7 +28,7 @@ ROOT_DIR="$(git rev-parse --show-toplevel)"
 COVERAGE_DIR="${COVERAGE_DIR:-${ROOT_DIR}/coverage}"
 COVERAGE_THRESHOLD="${COVERAGE_THRESHOLD:-0}"
 COVERAGE_STRICT="${COVERAGE_STRICT:-false}"
-COVERAGE_TARGETS="${COVERAGE_TARGETS:-tests/cli/test_main.bats tests/cli/test_all.sh tests/cli/test_install.bats tests/core/test_config.bats tests/core/test_planner.bats tests/tools/test_tools_registry.bats}"
+COVERAGE_TARGETS="${COVERAGE_TARGETS:-tests/cli/test_main.sh tests/cli/test_all.sh tests/cli/test_install.sh tests/core/test_config.sh tests/core/test_planner.sh tests/tools/test_tools_registry.sh}"
 
 export COVERAGE_DIR
 export SIMPLECOV_ROOT="${ROOT_DIR}"
